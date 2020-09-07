193963465e268b5be4fc1922bf3099a71babf9d2
Madhumidha K <madhumk2@in.ibm.com>
2020-09-08 03:19:28 +0530

    JS

--- index.js
+++ index.js
@@ -2,35 +2,36 @@ AOS.init({
   duration: 500,
 });
 
+$(document).ready(function(){
+      const $menuCheck = document.querySelector('.menu-checkbox');
+      $menuCheck.addEventListener('click', function(){
+        $($menuCheck).toggleClass('checked')
+      })
+      const $sidenavs = document.querySelectorAll('.side-nav');
+      $sidenavs.forEach((link) => {
+        link.addEventListener('click', function(){
+          $menuCheck.classList.remove('checked')
+        })
+      })
 
-const $menuCheck = document.querySelector('.menu-checkbox');
-$menuCheck.addEventListener('click', function(){
-  $($menuCheck).toggleClass('checked')
-})
-const $sidenavs = document.querySelectorAll('.side-nav');
-$sidenavs.forEach((link) => {
-  link.addEventListener('click', function(){
-     $menuCheck.classList.remove('checked')
-  })
-})
-
-const $resume = document.querySelector('#resume');
-const $displayEducation = document.querySelector('.display-education');
-const $education = document.querySelector('#education'); 
-const $displayWork = document.querySelector('.display-work');
-const $work = document.querySelector('#work');
+      const $resume = document.querySelector('#resume');
+      const $displayEducation = document.querySelector('.display-education');
+      const $education = document.querySelector('#education'); 
+      const $displayWork = document.querySelector('.display-work');
+      const $work = document.querySelector('#work');
 
 
-$displayEducation.addEventListener('click', function(){
-        $work.className = 'd-none';
-        $displayWork.classList.remove('active');
-        $displayEducation.classList.add('active');
-        $education.classList.remove('d-none');
-})
+      $displayEducation.addEventListener('click', function(){
+              $work.className = 'd-none';
+              $displayWork.classList.remove('active');
+              $displayEducation.classList.add('active');
+              $education.classList.remove('d-none');
+      })
 
-$displayWork.addEventListener('click', function(){
-  $education.className = 'd-none';
-  $displayEducation.classList.remove('active');
-  $displayWork.classList.add('active');
-  $work.classList.remove('d-none');
+      $displayWork.addEventListener('click', function(){
+        $education.className = 'd-none';
+        $displayEducation.classList.remove('active');
+        $displayWork.classList.add('active');
+        $work.classList.remove('d-none');
+      })
 })
