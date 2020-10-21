14400dd894a4c0b4882c16e57ba14c4692002753
Madhumidha K <madhumk2@in.ibm.com>
2020-10-22 03:38:13 +0530

    css

--- index.js
+++ index.js
@@ -23,15 +23,52 @@ $(document).ready(function(){
 
       $displayEducation.addEventListener('click', function(){
               $work.className = 'd-none';
-              $displayWork.classList.remove('active');
-              $displayEducation.classList.add('active');
+              $displayWork.classList.remove('current');
+              $displayEducation.classList.add('current');
               $education.classList.remove('d-none');
       })
 
       $displayWork.addEventListener('click', function(){
         $education.className = 'd-none';
-        $displayEducation.classList.remove('active');
-        $displayWork.classList.add('active');
+        $displayEducation.classList.remove('current');
+        $displayWork.classList.add('current');
         $work.classList.remove('d-none');
       })
 })
+
+
+let mainNavLinks = document.querySelectorAll(".top-nav-link");
+let mainSections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+  mainNavLinks.forEach(link => {
+    if(link.hash){
+      let section = document.querySelector(link.hash);
+    if (  
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight - 150 > fromTop || 
+        isInViewport(section)
+      ) {
+        document.querySelector(".active").classList.remove("active");
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    }
+  });
+});
+
+
+function isInViewport(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
